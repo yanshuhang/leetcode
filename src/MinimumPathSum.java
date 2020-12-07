c4277dbe16d2c28678b805038abe2041ba73140c
yanshuhang <sytem.out@hotmail.com>
2020-12-07 23:28:08 +0800

    最小路径和

---
+++ src/MinimumPathSum.java
@@ -0,0 +1,23 @@
+public class MinimumPathSum {
+    public int solution(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int column = grid[0].length;
+        int[][] dp = new int[row][column];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < column; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i - 1];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][column - 1];
+    }
+}
