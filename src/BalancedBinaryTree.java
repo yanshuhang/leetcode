2dcf9241395aa4916f8090f290ae23a25de5bca0
yanshuhang <sytem.out@hotmail.com>
2020-12-16 20:37:52 +0800

    平衡二叉树自底向上

--- src/BalancedBinaryTree.java
+++ src/BalancedBinaryTree.java
@@ -12,4 +12,20 @@ public class BalancedBinaryTree {
         }
         return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
     }
+
+    public boolean solution1(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
