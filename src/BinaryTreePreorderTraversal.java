e163631b4d78fd70a12aa64be0e695622f9b2916
yanshuhang <sytem.out@hotmail.com>
2020-12-17 21:33:44 +0800

    二叉树的前序遍历,迭代

--- src/BinaryTreePreorderTraversal.java
+++ src/BinaryTreePreorderTraversal.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTreePreorderTraversal {
+    // 递归
     public List<Integer> solution(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         preorder(root, ans);
@@ -14,5 +14,26 @@ public class BinaryTreePreorderTraversal {
             preorder(root.left, ans);
             preorder(root.right, ans);
         }
+    }
+
+    // 迭代
+    public List<Integer> solution1(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        List<Integer> ans = new ArrayList<>();
+        deque.push(root);
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.pop();
+            ans.add(node.val);
+            if (node.right != null) {
+                deque.push(node.right);
+            }
+            if (node.left != null) {
+                deque.push(node.left);
+            }
+        }
+        return ans;
     }
 }
