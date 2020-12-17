ce6c2bdbb42e898a6e1f71f6ba0d898e3988d5c8
yanshuhang <sytem.out@hotmail.com>
2020-12-17 22:57:30 +0800

    二叉树的后序遍历,迭代

--- src/BinaryTreePostorderTraversal.java
+++ src/BinaryTreePostorderTraversal.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTreePostorderTraversal {
     // 递归
@@ -16,5 +15,32 @@ public class BinaryTreePostorderTraversal {
             ans.add(root.val);
         }
     }
-
+    // 迭代
+    public List<Integer> solution1(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> ans = new ArrayList<>();
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        // 上次遍历的节点
+        TreeNode prev = null;
+        while (root != null || !deque.isEmpty()) {
+            while (root != null) {
+                deque.push(root);
+                root =root.left;
+            }
+            // 左节点到底后,如果没有右节点或者上次弹出的节点是右节点则当前节点可记录
+            // 否则当前节点继续压入,再往其右子树找到最左节点
+            root = deque.pop();
+            if (root.right == null || root.right == prev) {
+                ans.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                deque.push(root);
+                root = root.right;
+            }
+        }
+        return ans;
+    }
 }
