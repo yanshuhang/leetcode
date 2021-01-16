67935ba919c45157caafdc5d6588128b923eae31
yanshuhang <sytem.out@hotmail.com>
2021-01-16 21:00:48 +0800

    验证回文串

---
+++ src/ValidPalidrome.java
@@ -0,0 +1,23 @@
+public class ValidPalidrome {
+    public boolean solution(String s) {
+        int len = s.length();
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
