3213509ecfdd02681396af41cbf0d4de5c9a2d96
yanshuhang <sytem.out@hotmail.com>
2021-01-24 18:35:16 +0800

    买卖股票的最佳时机III

--- src/BestTimeToBuyAndSellStock3.java
+++ src/BestTimeToBuyAndSellStock3.java
@@ -11,7 +11,7 @@ public class BestTimeToBuyAndSellStock3 {
             buy1 = Math.max(buy1, -prices[i]);
             sell1 = Math.max(sell1, buy1 + prices[i]);
             buy2 = Math.max(buy2, sell1 - prices[i]);
-            sell2 = Math.max(sell2, sell2 + prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
         }
         return sell2;
     }
