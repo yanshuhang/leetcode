db44c1b9034f86ef1bff23e785e7743dd135fce8
yanshuhang <sytem.out@hotmail.com>
2021-02-12 22:30:04 +0800

    乘积最大子数组:动态规划

--- src/MaximumProductSubarray.java
+++ src/MaximumProductSubarray.java
@@ -13,8 +13,24 @@ public class MaximumProductSubarray {
         return ans;
     }
 
+    public int solution1(int[] nums) {
+        // 一次遍历: 维护两个变量, 已计算乘积的最大值和最小值
+        // 如果当前是正数则最大值是乘以最大值, 如果是负数则是乘以最小值
+        int ans = nums[0];
+        int maxPre = nums[0];
+        int minPre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 由于maxPre在计算后会变,所以需要记录下来用以minPre的计算
+            int n = maxPre;
+            maxPre = Math.max(maxPre * nums[i], Math.max(nums[i], minPre * nums[i]));
+            minPre = Math.min(minPre * nums[i], Math.min(nums[i], n * nums[i]));
+            ans = Math.max(ans, maxPre);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {-2,0,1};
-        System.out.println(new MaximumProductSubarray().solution(nums));
+        int[] nums = {-4,-3,-2};
+        System.out.println(new MaximumProductSubarray().solution1(nums));
     }
 }
