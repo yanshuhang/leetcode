126036fc4662c8055261004f75a435c25e11bfe5
yanshuhang <sytem.out@hotmail.com>
2021-02-18 20:34:21 +0800

    寻找旋转排序数组中的最小值II

---
+++ src/FindMinimumInRotatedSortedArray2.java
@@ -0,0 +1,17 @@
+public class FindMinimumInRotatedSortedArray2 {
+    public int solution(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+}
