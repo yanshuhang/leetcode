1e3c890741b39f61ed22ae8b23605ba256e01db8
yanshuhang <sytem.out@hotmail.com>
2021-02-26 23:36:38 +0800

    多数元素

---
+++ src/MajorityElement.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class MajorityElement {
+    public int solution(int[] nums) {
+        // 排序版
+        Arrays.sort(nums);
+        return nums[nums.length >> 1];
+    }
+
+    public int solution1(int[] nums) {
+        // 投票法
+        // 两个不同的数抵消, 剩余的最后的数一定是众数, 因为众数肯定不会被抵消完
+        int num = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (num == nums[i]) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    num = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return num;
+    }
+}
