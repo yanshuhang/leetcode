e4e964ecb2e30d6b788bb1c38cb54bc7ca2f465e
yanshuhang <sytem.out@hotmail.com>
2021-05-02 01:01:19 +0800

    快乐数

---
+++ src/HappyNumber.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public boolean solution(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            n = getNext(n);
+            set.add(n);
+        }
+        return n == 1;
+    }
+
+    public int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+            sum += digit * digit;
+        }
+        return sum;
+    }
+}
