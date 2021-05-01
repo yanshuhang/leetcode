e9fb8bc809b745720731da5470f3f1378f3552d7
yanshuhang <sytem.out@hotmail.com>
2021-05-02 05:22:42 +0800

    课程表：广度优先遍历

---
+++ src/CourseSchedule.java
@@ -0,0 +1,42 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseSchedule {
+    public boolean solution(int numCourses, int[][] prerequisites) {
+        // 广度遍历
+        List<List<Integer>> edges = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+
+        // 记录每个点的出度列表和入读数量
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+            indegree[info[0]]++;
+        }
+
+        // 入度为0的放入队列
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            visited++;
+            int course = queue.poll();
+            for (int next : edges.get(course)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+        return visited == numCourses;
+    }
+}
