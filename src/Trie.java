7cfc31e6ac6ea5dd3bd51304860b0f23d843f378
yanshuhang <sytem.out@hotmail.com>
2021-05-03 21:35:26 +0800

    前缀树 trie

---
+++ src/Trie.java
@@ -0,0 +1,45 @@
+public class Trie {
+    private Trie[] children;
+    private boolean end;
+
+    public Trie() {
+        children = new Trie[26];
+        end = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.end = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.end;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    private Trie searchPrefix(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
