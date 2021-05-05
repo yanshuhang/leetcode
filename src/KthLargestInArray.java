455dca38dbc45c8ea2090e37354bf7de4d947ba9
yanshuhang <sytem.out@hotmail.com>
2021-05-05 15:47:43 +0800

    数组中的第k个最大的元素：使用最小堆

--- src/KthLargestInArray.java
+++ src/KthLargestInArray.java
@@ -1,47 +1,33 @@
 public class KthLargestInArray {
     public int solution(int[] nums, int k) {
-        int heapSize = nums.length;
-        buildMaxHeap(nums, heapSize);
-        // 找出前k-1个最大数
-        for (int i = nums.length - 1; i >= nums.length - k + 1; i--) {
-            swap(nums, 0, i);
-            heapSize--;
-            heapify(nums, 0, heapSize);
+        // 使用最小堆
+        int[] heap = new int[k];
+        for (int i = 0; i < k; i++) {
+            heap[i] = Integer.MIN_VALUE;
         }
-        return nums[0];
+        for (int num : nums) {
+            if (num > heap[0])
+                heapify(heap, num, k);
+        }
+        return heap[0];
     }
 
-    // 从非叶节点开始向下调整，创建最大堆
-    public void buildMaxHeap(int[] a, int heapSize) {
-        int i = (heapSize >>> 1) - 1;
-        for (; i >= 0; i--) {
-            heapify(a, i, heapSize);
-        }
-    }
-
-    // 堆节点向下调整, 遍历版本
-    public void heapify(int[] a, int i, int heapSize) {
+    public void heapify(int[] a, int key, int heapSize) {
         int half = heapSize >>> 1;
-        int key = a[i];
-        while (i < half) {
-            int child = (i << 1) + 1;
+        int k = 0;
+        while (k < half) {
+            int child = (k << 1) + 1;
             int right = child + 1;
             int c = a[child];
-            if (right < heapSize && c < a[right]) {
+            if (right < heapSize && c > a[right]) {
                 c = a[child = right];
             }
-            if (key > c) {
+            if (key < c) {
                 break;
             }
-            a[i] = c;
-            i = child;
+            a[k] = c;
+            k = child;
         }
-        a[i] = key;
-    }
-
-    public void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+        a[k] = key;
     }
 }
