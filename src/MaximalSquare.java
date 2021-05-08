3bdfb6f48c8a8147938ad734d4ea86938c229dfc
yanshuhang <sytem.out@hotmail.com>
2021-05-08 12:38:49 +0800

    最大正方形

---
+++ src/MaximalSquare.java
@@ -0,0 +1,59 @@
+public class MaximalSquare {
+    // 暴力法
+    public int solution(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int maxSide = 0;
+        int rows = matrix.length;
+        int colums = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colums; j++) {
+                if (matrix[i][j] == '1') {
+                    maxSide = Math.max(maxSide, 1);
+                    int currentMaxSide = Math.min(rows - i - 1, colums - j - 1);
+                    for (int k = 1; k <= currentMaxSide; k++) {
+                        boolean flag = true;
+                        for (int m = 0; m <= k; m++) {
+                            if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            maxSide = Math.max(maxSide, k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    // 动态规划：以当前点为右下角的正方形
+    public int solution1(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int colums = matrix[0].length;
+        int[][] dp = new int[rows][colums];
+        int maxSide = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < colums; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i-1][j-1]) + 1;
+                    }
+                }
+                maxSide = Math.max(maxSide, dp[i][j]);
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
