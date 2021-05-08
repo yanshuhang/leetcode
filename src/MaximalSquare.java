d043c7885ec15b875536ff4dc219afe38ed70bbb
yanshuhang <sytem.out@hotmail.com>
2021-05-08 12:46:02 +0800

    最大正方形：优化逻辑判断

--- src/MaximalSquare.java
+++ src/MaximalSquare.java
@@ -40,18 +40,14 @@ public class MaximalSquare {
         }
         int rows = matrix.length;
         int colums = matrix[0].length;
-        int[][] dp = new int[rows][colums];
+        int[][] dp = new int[rows + 1][colums + 1];
         int maxSide = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < colums; j++) {
                 if (matrix[i][j] == '1') {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i-1][j-1]) + 1;
-                    }
+                    dp[i + 1][j + 1] = Math.min(Math.min(dp[i + 1][j], dp[i][j + 1]), dp[i][j]) + 1;
                 }
-                maxSide = Math.max(maxSide, dp[i][j]);
+                maxSide = Math.max(maxSide, dp[i + 1][j + 1]);
             }
         }
         return maxSide * maxSide;
