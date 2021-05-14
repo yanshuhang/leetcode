27e7cc92fadddfb797e588db48b20772969abc18
yanshuhang <sytem.out@hotmail.com>
2021-05-14 22:35:35 +0800

    求众数：使用Stream和lambda

--- src/MajorityElement2.java
+++ src/MajorityElement2.java
@@ -1,20 +1,13 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MajorityElement2 {
     public List<Integer> solution(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.merge(num, 1, Integer::sum);
-        }
-        List<Integer> ans = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > nums.length / 3) {
-                ans.add(entry.getKey());
-            }
-        }
-        return ans;
+        Map<Integer, Integer> map = Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.toMap(Function.identity(), i -> 1, Integer::sum));
+        int n = nums.length / 3;
+        return map.entrySet().stream().filter(e -> e.getValue() > n).map(Map.Entry::getKey).collect(Collectors.toList());
     }
 }
