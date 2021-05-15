5be0070bf7c4d0b61e737754deb08667a1a4cfb6
yanshuhang <sytem.out@hotmail.com>
2021-05-15 14:30:19 +0800

    求众数：遍历投票

--- src/MajorityElement2.java
+++ src/MajorityElement2.java
@@ -10,4 +10,59 @@ public class MajorityElement2 {
         int n = nums.length / 3;
         return map.entrySet().stream().filter(e -> e.getValue() > n).map(Map.Entry::getKey).collect(Collectors.toList());
     }
+
+    public List<Integer> solution1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return Collections.emptyList();
+        }
+        List<Integer> ans = new ArrayList<>();
+        int cand1 = nums[0];
+        int cand2 = nums[0];
+        int count1 = 0;
+        int count2 = 0;
+        for (int num : nums) {
+            if (num == cand1) {
+                count1++;
+                continue;
+            }
+            if (num == cand2) {
+                count2++;
+                continue;
+            }
+            if (count1 == 0) {
+                cand1 = num;
+                count1++;
+                continue;
+            }
+            if (count2 == 0) {
+                cand2 = num;
+                count2++;
+                continue;
+            }
+            count1--;
+            count2--;
+        }
+        count1 = 0;
+        count2 = 0;
+        int n = nums.length / 3;
+        for (int num : nums) {
+            if (num == cand1) {
+                count1++;
+            } else if (num == cand2) {
+                count2++;
+            }
+        }
+        if (count1 > n) {
+            ans.add(cand1);
+        }
+        if (count2 > n) {
+            ans.add(cand2);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2};
+        System.out.println(new MajorityElement2().solution1(nums));
+    }
 }
