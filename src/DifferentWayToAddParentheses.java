61ad8ddd70f32184cc4fe4b587b67656c07a0ff3
yanshuhang <sytem.out@hotmail.com>
2021-05-20 21:59:49 +0800

    为运算表达式设计优先级：递归

---
+++ src/DifferentWayToAddParentheses.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DifferentWayToAddParentheses {
+    public List<Integer> solution(String expression) {
+        if (expression == null || expression.length() == 0) {
+            return Collections.emptyList();
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '+' || c == '-' || c == '*') {
+                // 递归子问题
+                List<Integer> leftList = solution(expression.substring(0, i));
+                List<Integer> rightList = solution(expression.substring(i + 1));
+                for (Integer leftValue : leftList) {
+                    for (Integer rightValue : rightList) {
+                        if (c == '+') {
+                            ans.add(leftValue + rightValue);
+                        } else if (c == '-') {
+                            ans.add(leftValue - rightValue);
+                        } else {
+                            ans.add(leftValue * rightValue);
+                        }
+                    }
+                }
+            }
+        }
+        if (ans.isEmpty()) {
+            ans.add(Integer.valueOf(expression));
+        }
+        return ans;
+    }
+}
