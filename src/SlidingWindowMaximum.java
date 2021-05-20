7451a50bffc9a48c6a3f0a22b15c9853863b4a69
yanshuhang <sytem.out@hotmail.com>
2021-05-20 20:26:43 +0800

    滑动窗口最大值：使用双端队列

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Objects;
 import java.util.PriorityQueue;
 
@@ -17,6 +19,35 @@ public class SlidingWindowMaximum {
                 queue.poll();
             }
             ans[i-k+1] = Objects.requireNonNull(queue.peek())[0];
+        }
+        return ans;
+    }
+
+    public int[] solution1(int[] nums, int k) {
+        int len = nums.length;
+        int[] ans = new int[len - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        // 存储可能是最大值的索引，如果右侧有比其大的数，说明当前不可能是最大数
+        for (int i = 0; i < k; i++) {
+            // 去除右侧比当前值小的数
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        ans[0] = nums[Objects.requireNonNull(deque.peekFirst())];
+        // 窗口向右滑动
+        for (int i = k; i < len; i++) {
+            // 去除不可能是最大的数
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            // 最大的数有可能不在窗口内
+            while (Objects.requireNonNull(deque.peekFirst()) <= i - k) {
+                deque.pollFirst();
+            }
+            ans[i-k+1] = nums[deque.peekFirst()];
         }
         return ans;
     }
