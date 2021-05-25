2cbbb8cf1a5dbf34a24c9ea5df1a37513387ab25
yanshuhang <sytem.out@hotmail.com>
2021-05-25 19:35:42 +0800

    缺失的数字：异或位运算

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -2,8 +2,18 @@ import java.util.Arrays;
 
 public class MissingNumber {
     public int solution(int[] nums) {
+        // 求和
         int sum = Arrays.stream(nums).sum();
         int n = nums.length;
         return (n * (n + 1)) / 2 - sum;
+    }
+
+    public int solution1(int[] num) {
+        // 异或：可以在一群偶数个的数中找到单独的那个奇数个的数字
+        int missing = num.length;
+        for (int i = 0; i < num.length; i++) {
+            missing ^= i ^ num[i];
+        }
+        return missing;
     }
 }
