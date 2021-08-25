0fb60bc8b3592f9e94e860a74e121e590127bcf0
yanshuhang <sytem.out@hotmail.com>
2021-08-26 03:21:17 +0800

    移动零：替换

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -10,4 +10,22 @@ public class MoveZeroes {
             nums[i] = 0;
         }
     }
+
+    public void solution1(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
 }
